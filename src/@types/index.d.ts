234a1342b881a529dea4f57a9dab8afc2c580f63
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-01-27 05:14:10 -0300

    Revert "dealing fonts - error on tsconfig"
    This reverts commit 541de1b88f8fb98512d45164cd91c15b8544a9b4.

--- src/@types/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.png';
-/**
- * Essa declaração faz com que os arquivos .png
- * sejam tratados como módulos, então não teremos
- * problemas com a logo que possui 3 nomes
- * para as diferentes densidades
- * */
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Image } from 'react-native';
 
 import { Container } from './styles';
 
-import logoImg from '../../assets/logo.png';
-
 const SignIn: React.FC = () => {
-  return (
-    <Container>
-      <Image source={logoImg} />
-    </Container>
-  );
+  return <Container />;
 };
 
 export default SignIn;

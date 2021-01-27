541de1b88f8fb98512d45164cd91c15b8544a9b4
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-01-27 05:08:24 -0300

    dealing fonts - error on tsconfig

---
+++ src/@types/index.d.ts
@@ -0,0 +1,7 @@
+declare module '*.png';
+/**
+ * Essa declaração faz com que os arquivos .png
+ * sejam tratados como módulos, então não teremos
+ * problemas com a logo que possui 3 nomes
+ * para as diferentes densidades
+ * */

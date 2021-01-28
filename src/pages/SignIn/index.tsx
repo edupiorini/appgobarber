cd5d3f02eda2666670d559bc6e8ddba6a34f321a
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-01-28 08:18:35 -0300

    forgot password and create account styled and inserted

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -7,25 +8,45 @@ import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
 
 import { Container, Title } from './styles';
+import {
+  CreateAccountButton,
+  CreateAccountButtonText,
+  ForgotPassword,
+  ForgotPasswordText,
+} from '../../components/Button/style';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
+    <>
+      <Container>
+        <Image source={logoImg} />
+        <Title>Faça seu logon</Title>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+        <Button
+          onPress={() => {
+            // eslint-disable-next-line no-console
+            console.log('Deu');
+          }}
+        >
+          Entrar
+        </Button>
 
-      <Button
-        onPress={() => {
-          // eslint-disable-next-line no-console
-          console.log('Deu');
-        }}
-      >
-        Entrar
-      </Button>
-    </Container>
+        <ForgotPassword
+          onPress={() => {
+            // eslint-disable-next-line no-console
+            console.log('esqueci');
+          }}
+        >
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
